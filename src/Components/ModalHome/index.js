cf67d3838672ac6b96b98604f77d51054293bbc2
Caio <caiolucasrdz@gmail.com>
2024-05-07 22:18:41 -0300

    configurada checkbox de compra parcelada

--- src/Components/ModalHome/index.js
+++ src/Components/ModalHome/index.js
@@ -1,18 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import Modal from 'react-modal';
 import { PlusCircleOutlined } from '@ant-design/icons';
 import * as Animatable from 'react-native-animatable';
+import axios from 'axios';
 
 Modal.setAppElement('#root');
 
 export default function ModalHome() {
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [local, setLocal] = useState('');
+  const [Local, setLocal] = useState('');
   const [descricao, setDescricao] = useState('');
   const [parcela, setParcela] = useState('');
   const [valorParcela, setValorParcela] = useState('');
   const [valorGasto, setValorGasto] = useState('');
+  const [eparcela, setEParcela] = useState(false);
+
+  const handleSalvarGasto = () => {
+    if (!Local || !descricao || (eparcela && (!parcela || !valorParcela)) || (!eparcela && !valorGasto)) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+  
+    const dados = {
+      valor: eparcela ? '' : valorGasto, // Se for parcelado, o valor do gasto é vazio
+      valorParcela: valorParcela,
+      parcela: parcela,
+      descricao: descricao,
+      Local: Local,
+    };
+  
+    axios.post('http://localhost:8080/user/gasto', dados)
+      .then(response => {
+        console.log('Resposta do servidor:', response);
+      })
+      .catch(error => {
+        console.error('Erro ao enviar requisição:', error);
+      });
+
+      fecharModal();
+  };
 
   function abrirModal() {
     setIsOpen(true);
@@ -22,16 +49,23 @@ export default function ModalHome() {
     setIsOpen(false);
   }
 
-  function handleSalvarGasto() {
-    // Aqui você pode adicionar a lógica para salvar os dados do gasto
-    console.log('Local:', local);
-    console.log('Descrição:', descricao);
-    console.log('Parcela:', parcela);
-    console.log('Valor da Parcela:', valorParcela);
-    console.log('Valor do Gasto:', valorGasto);
-    // Depois de salvar, você pode fechar o modal
-    fecharModal();
-  }
+  useEffect(() => {
+    if (eparcela && parcela && valorParcela) {
+      const valorCalculado = parseFloat(parcela) * parseFloat(valorParcela);
+      setValorGasto(valorCalculado.toString());
+    }
+  }, [parcela, valorParcela, eparcela]);
+
+  // function handleSalvarGasto() {
+  //   // Aqui você pode adicionar a lógica para salvar os dados do gasto
+  //   console.log('Local:', local);
+  //   console.log('Descrição:', descricao);
+  //   console.log('Parcela:', parcela);
+  //   console.log('Valor da Parcela:', valorParcela);
+  //   console.log('Valor do Gasto:', valorGasto);
+  //   // Depois de salvar, você pode fechar o modal
+  //   fecharModal();
+  // }
 
   return (
     <View style={styles.container}>
@@ -69,7 +103,7 @@ export default function ModalHome() {
           <TextInput
             placeholder='Digite o local'
             style={styles.input}
-            value={local}
+            value={Local}
             onChangeText={text => setLocal(text)}
           />
 
@@ -81,28 +115,49 @@ export default function ModalHome() {
             onChangeText={text => setDescricao(text)}
           />
 
-          <Text style={styles.title}>Parcela:</Text>
-          <TextInput
-            placeholder='Digite a parcela'
-            style={styles.input}
-            value={parcela}
-            onChangeText={text => setParcela(text)}
-          />
+          {eparcela && ( // Renderiza o campo de parcela apenas se a compra for parcelada
+            <>
+              <Text style={styles.title}>Parcela:</Text>
+              <TextInput
+                placeholder='Digite a parcela'
+                style={styles.input}
+                value={parcela}
+                onChangeText={text => setParcela(text)}
+                keyboardType='decimal-pad'
+              />
 
-          <Text style={styles.title}>Valor da Parcela:</Text>
-          <TextInput
-            placeholder='Digite o valor da parcela'
-            style={styles.input}
-            value={valorParcela}
-            onChangeText={text => setValorParcela(text)}
-          />
+              <Text style={styles.title}>Valor da Parcela:</Text>
+              <TextInput
+                placeholder='Digite o valor da parcela'
+                style={styles.input}
+                value={valorParcela}
+                onChangeText={text => setValorParcela(text)}
+                keyboardType='decimal-pad'
+              />
+            </>
+          )}
+
+
+
+          <View style={styles.checkboxContainer}>
+            <Text style={styles.checkboxLabel}>Compra parcelada?</Text>
+            <TouchableOpacity
+              onPress={() => setEParcela(!eparcela)}
+              style={[styles.checkbox, eparcela && styles.checkboxSelected]}
+            >
+              {eparcela && <Text style={styles.checkboxText}>✔</Text>} {/* Ícone de marca de seleção quando a compra for parcelada */}
+              {!eparcela && <Text style={styles.checkboxText}>❌</Text>} {/* Ícone de marca de seleção vazio quando a compra não for parcelada */}
+            </TouchableOpacity>
+          </View>
 
           <Text style={styles.title}>Valor do Gasto:</Text>
           <TextInput
             placeholder='Digite o valor do gasto'
-            style={styles.input}
+            style={[styles.input, eparcela && styles.inputDisabled]}
             value={valorGasto}
             onChangeText={text => setValorGasto(text)}
+            editable={!eparcela}
+            keyboardType='decimal-pad'
           />
 
           <TouchableOpacity style={styles.button} onPress={handleSalvarGasto}>
@@ -132,7 +187,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     padding: 20,
     right: 20, // Ajusta a posição do botão para a esquerda
-    top: 800, // Ajusta a posição do botão para cima
+    top: 1000, // Ajusta a posição do botão para cima
     zIndex: 10,
   },
   addButtonIcon: {
@@ -189,5 +244,27 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  checkboxSelected: {
+    backgroundColor: '#198754', // Cor de fundo quando selecionado
+    borderWidth: 2, // Largura da borda quando selecionado
+  },
+  checkboxContainer: {
+    flexDirection: 'row', // Alinha os itens horizontalmente
+    alignItems: 'center', // Alinha os itens verticalmente
+    marginBottom: 12, // Espaçamento inferior
+  },
+  checkboxLabel: {
+    fontSize: 16, // Tamanho do texto
+    marginRight: 10, // Espaçamento à direita do texto
+  },
+  checkboxText: {
+    fontSize: 20, // Tamanho do texto
+    color: '#FFF', // Cor do texto
+    marginLeft: 5, // Espaçamento à esquerda do texto
+  },
+  inputDisabled: {
+    backgroundColor: '#f5f5f5', // Cor de fundo quando desabilitado
+    color: '#999', // Cor do texto quando desabilitado
   },
 });
